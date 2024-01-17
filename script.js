447fd9680648308bf6b6fdb4e2d0044e89708ef6
mad88era <150746505+mad88era@users.noreply.github.com>
2024-01-16 18:57:45 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -4,24 +4,24 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 //Define a function named writePassword
 function writePassword() {
-// call the generatepassword function and store the result in the variable password
-  var password = generatePassword();
-  //Select the HTMl element with the id "password" and store it in the variable passwordText
-  var passwordText = document.querySelector("#password");
+    // call the generatepassword function and store the result in the variable password
+    var password = generatePassword();
+    //Select the HTMl element with the id "password" and store it in the variable passwordText
+    var passwordText = document.querySelector("#password");
 
-  //Set the value of the passwordText input field  to the generated password
-  passwordText.value = password;
+    //Set the value of the passwordText input field  to the generated password
+    passwordText.value = password;
 }
 
 //add an event listener to the generated element. Listening for a "click" event. and calling the writePassword function
 //Define a function named generatePassword
-generateBtn.addEventListener("Click", writePassword);
+generateBtn.addEventListener("click", writePassword);
 
 //Add a function to generate a password based on the user prompts
 //Define function named generatePassword
-function generatePassword () {
+function generatePassword() {
   //Call the promptPasswordlength function and store the result in the variable passwordLength
-  var passwordLength = promptPasswordlength();
+  var passwordLength = promptPasswordLength();
   // Validate password length using IF the password length is empty, then is not equal to the isNaN determines if its a value. Ensures that the password length is greater than eight characters and less than 128.
   // password length is greater than eight characters or less than 128
   if (passwordLength === null || isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
@@ -33,34 +33,34 @@ function generatePassword () {
 
   //Prompt for character types
   // These VAR declare the function called includeLowercase initializing the confirm method displaying the message "include lowercase characters?"
-  var includeLowercase = confirm("Include lowercase Characters?");
+  var includeLowercase = confirm("Include lowercase characters?");
   // These VAR declare the function called includeLowercase initializing the confirm method displaying the message "include uppercase characters?"
-  var includeUppercase = confirm("Include uppercase Characters?");
+  var includeUppercase = confirm("Include uppercase characters?");
   // These VAR declare the function called includeLowercase initializing the confirm method displaying the message "include numeric characters?"
-  var includeNumeric = confirm("Include numeric Characters?");
+  var includeNumeric = confirm("Include numeric characters?");
   // These VAR declare the function called includeLowercase initializing the confirm method displaying the message "include special characters?"
-  var includeSpecial = confirm("Include special Characters?");
+  var includeSpecial = confirm("Include special characters?");
 
 
   //Validate that at least one of the character types is selected
   //using the If statement and the logical NOT "!" verifies if the request does not include a lowercase, uppercase, numeric or special characters.
   if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
-    //if the criterias arenot met then return the message "Please select at least one character type"
+    //if the criterias are not met then return the message "Please select at least one character type"
     alert("Please select at least one character type.");
-    //return end the function and specifies a value to the function caller
+    //return end the fuction and specifies a value tot he function caller
     return "";
   }
 
   // Generate password based on selected criteria, this will be used later in the loop
   // This variable called password using the function "generateRandomPassword" that meets the criteria of length, lowercase, uppercase, numeric values or special characters
   var password = generateRandomPassword(passwordLength, includeLowercase, includeUppercase, includeNumeric, includeSpecial);
-
+  // then return the password. 
   return password;
 }
 
 //Functionj to prompt password lenghth
 //This function is to prompt the box as the password length
-function promptPasswordlength() {
+function promptPasswordLength() {
   //This calls for valuable to enter the length entry, then using the prompt it displays the message
   var lengthInput = prompt("Enter the length of the password (between 8 and 128 characters):");
   //return end the prompt by using pareInt analizes the string and returns the number.
@@ -101,22 +101,19 @@ function generateRandomPassword(length, includeLowercase, includeUppercase, incl
   if (includeSpecial) {
     charactersSet += specialCharacters;
   }
-
   //Generate the password
   //Use the variable password to equal the result of the FOR loop specified below
   var password = "";
   // for the variable used in the interator "i" to equal 0, then iteration i is the length then use increment operator i++
-  for (var i = o; i < length; i++) {
+  for (var i = 0; i < length; i++) {
     //use the variable randomIndex to return a number using the math.floor then using Math.random look at the character set 
     //in characterSet then the length specified by the user
     var randomIndex = Math.floor(Math.random() * charactersSet.length);
     //set the password to add the characterset and sets a set of string called randomIndex.
     password += charactersSet.charAt(randomIndex);
   }
-
 //this ends the generateRandomPassword returning the password
 return password;
-
 }
 
 
